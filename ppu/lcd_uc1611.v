765aca5375b525a3d03dd8d16c65770528462c1a
Michael Singer <michael@a-singer.de>
2018-04-10 22:06:11 +0200

    display init/uninit

--- ppu/lcd_uc1611.v
+++ ppu/lcd_uc1611.v
@@ -22,45 +22,33 @@ module uc1611(
 	reg [1:0]  state, new_state;
 	reg [15:0] count, new_count;
 
-	reg new_lcd_write, new_lcd_cs;
+	reg [7:0] new_lcd_data;
+	reg       new_lcd_cd, new_lcd_write, new_lcd_cs;
 
-	reg [8:0] init_seq[0:31];
+	reg [7:0] init_seq[0:15];
 	initial begin
-		init_seq[0]  <= 'h0d2; /* Set Gray Scale Control: LC[6:5]='b10 (16 colors) */
-		init_seq[1]  <= 'h0c0; /* Set LCD Mapping Control: MY=0 MX=0 MSF=0 */
-//		init_seq[1]  <= 'h0c2; /* Set LCD Mapping Control: MY=0 MX=1 MSF=0 */
-//		init_seq[1]  <= 'h0c4; /* Set LCD Mapping Control: MY=1 MX=0 MSF=0 */
-//		init_seq[1]  <= 'h0c6; /* Set LCD Mapping Control: MY=1 MX=1 MSF=0 */
-		init_seq[2]  <= 'h0ea; /* Set LCD Bias Ratio: BR[1:0]='b10 (11) */
-		init_seq[3]  <= 'h02a; /* Set Panel Loading: PC[1:0]='b10 (28~40nF) */
-		init_seq[4]  <= 'h081; /* Set Gain and Potentiometer: */
-		init_seq[5]  <= 'h046; /*   GN[1:0]='b01 (1) PM[5:0]='b000110 (6) */
-		init_seq[6]  <= 'h0a1; /* Set Line Rate: LC[4:3]='b01 (23.2kHz) */
-		init_seq[7]  <= 'h084; /* Set Partial Display Control: LC[9:8]='b00 (disable) */
-		init_seq[8]  <= 'h089; /* Set RAM Address Control: AC[2:0]='b001 (auto inc col, then page when col wraps) */
-		init_seq[9]  <= 'h031; /* Set Advanced Product Configuration: R=1 */
-		init_seq[10] <= 'h081; /*   APC='h81 */
-		init_seq[11] <= 'h0af; /* Set Display Enable: DC[4:2]='b111 (enable all three sets of columns) */
-		init_seq[12] <= 'h040; /* Set Scroll Line LSB: SL[3:0]=0 */
-		init_seq[13] <= 'h050; /* Set Scroll Line MSB: SL[7:4]=0 */
-		init_seq[14] <= 'h060; /* Set Page Address LSB: PA[3:0]=0 */
-		init_seq[15] <= 'h070; /* Set Page Address MSB: PA[6:4]=0 */
-		init_seq[16] <= 'h000; /* Set Column Address LSB: CA[3:0]=0 */
-		init_seq[17] <= 'h010; /* Set Column Address MSB: CA[7:4]=0 */
-		init_seq[18] <= 'h0e3; /* NOP */
-		init_seq[19] <= 'h0e3; /* NOP */
-		init_seq[20] <= 'h0e3; /* NOP */
-		init_seq[21] <= 'h0e3; /* NOP */
-		init_seq[22] <= 'h0e3; /* NOP */
-		init_seq[23] <= 'h102;//'h0e3; /* NOP */
-		init_seq[24] <= 'h146;//'h0e3; /* NOP */
-		init_seq[25] <= 'h18a;//'h0e3; /* NOP */
-		init_seq[26] <= 'h1ce;//'h0e3; /* NOP */
-		init_seq[27] <= 'h0e3; /* NOP */
-		init_seq[28] <= 'h0e3; /* NOP */
-		init_seq[29] <= 'h0e3; /* NOP */
-		init_seq[30] <= 'h0e3; /* NOP */
-		init_seq[31] <= 'h0e3; /* NOP */
+//		init_seq[0]  <= 'hc0; /* Set LCD Mapping Control: MY=0 MX=0 MSF=0 */
+//		init_seq[0]  <= 'hc2; /* Set LCD Mapping Control: MY=0 MX=1 MSF=0 */
+//		init_seq[0]  <= 'hc4; /* Set LCD Mapping Control: MY=1 MX=0 MSF=0 */
+		init_seq[0]  <= 'hc6; /* Set LCD Mapping Control: MY=1 MX=1 MSF=0 */
+		init_seq[1]  <= 'ha1; /* Set Line Rate: LC[4:3]='b01 (23.2kHz) */
+//		init_seq[1]  <= 'ha2; /* Set Line Rate: LC[4:3]='b10 (27.2kHz) */
+		init_seq[2]  <= 'h2a; /* Set Panel Loading: PC[1:0]='b10 (28~40nF) */
+		init_seq[3]  <= 'hd2; /* Set Gray Scale Control: LC[6:5]='b10 (16 colors) */
+		init_seq[4]  <= 'hea; /* Set LCD Bias Ratio: BR[1:0]='b10 (11) */
+		init_seq[5]  <= 'h81; /* Set Gain and Potentiometer: */
+//		init_seq[6]  <= 'h46; /*   GN[1:0]='b01 (1) PM[5:0]='b000110 (6) */
+		init_seq[6]  <= 'h00; /*   GN[1:0]='b00 (0) PM[5:0]='b000000 (0) */
+		init_seq[7]  <= 'h84; /* Set Partial Display Control: LC[9:8]='b00 (disable) */
+//		init_seq[8]  <= 'h89; /* Set RAM Address Control: AC[2:0]='b001 (auto inc col, then page when col wraps) */
+		init_seq[8]  <= 'h8b; /* Set RAM Address Control: AC[2:0]='b011 (auto inc page, then col when page wraps) */
+		init_seq[9]  <= 'haf; /* Set Display Enable: DC[4:2]='b111 (enable all three sets of columns) */
+		init_seq[10] <= 'h40; /* Set Scroll Line LSB: SL[3:0]=0 */
+		init_seq[11] <= 'h50; /* Set Scroll Line MSB: SL[7:4]=0 */
+		init_seq[12] <= 'h60; /* Set Page Address LSB: PA[3:0]=0 */
+		init_seq[13] <= 'h70; /* Set Page Address MSB: PA[6:4]=0 */
+		init_seq[14] <= 'h00; /* Set Column Address LSB: CA[3:0]=0 */
+		init_seq[15] <= 'h10; /* Set Column Address MSB: CA[7:4]=0 */
 	end
 
 	assign lcd_read = 0;
@@ -70,14 +58,17 @@ module uc1611(
 		new_state     = state;
 		new_count     = (state == `STATE_INIT || state == `STATE_UNINIT) ? (count + 1) : 'bx;
 
+		new_lcd_data  = lcd_data;
+		new_lcd_cd    = lcd_cd;
 		new_lcd_write = lcd_write;
 		new_lcd_cs    = lcd_cs;
 
 		case (state)
 		`STATE_OFF:
 			if (disp_on) begin
-				new_state = `STATE_INIT;
-				new_count = 0;
+				new_state  = `STATE_INIT;
+				new_count  = 0;
+				new_lcd_cd = 0;
 			end
 		`STATE_INIT:
 			if (&count[15:14]) begin
@@ -91,24 +82,40 @@ module uc1611(
 				3:
 					begin
 						new_lcd_cs = 0;
-						if (count[6:2] == 31)
+						if (count[5:2] == 15)
 							new_state = `STATE_ON;
 					end
 				endcase
 			end
 		`STATE_ON:
 			if (!disp_on) begin
-				new_state = `STATE_UNINIT;
-				new_count = 0;
+				new_state  = `STATE_UNINIT;
+				new_count  = 0;
+				new_lcd_cd = 0;
+				new_lcd_data = 'he2; /* System Reset */
 			end
 		`STATE_UNINIT:
-			new_state = `STATE_OFF;
+			case (count[1:0])
+			0:
+				new_lcd_cs = 1;
+			1:
+				new_lcd_write = 1;
+			2:
+				new_lcd_write = 0;
+			3:
+				begin
+					new_lcd_cs = 0;
+					new_state = `STATE_OFF;
+				end
+			endcase
 		endcase
 
 		if (reset) begin
 			new_state     = `STATE_OFF;
 			new_count     = 'bx;
 
+			new_lcd_data  = 'bx;
+			new_lcd_cd    = 'bx;
 			new_lcd_write = 0;
 			new_lcd_cs    = 0;
 		end
@@ -118,8 +125,11 @@ module uc1611(
 		state     <= new_state;
 		count     <= new_count;
 
-		{ lcd_cd, lcd_data } <= (state == `STATE_INIT && !count[1:0]) ? init_seq[count[6:2]] : 'bx;
+		lcd_data  <= new_lcd_data;
+		if (state == `STATE_INIT && !count[1:0])
+			lcd_data <= init_seq[count[5:2]];
 
+		lcd_cd    <= new_lcd_cd;
 		lcd_write <= new_lcd_write;
 		lcd_cs    <= new_lcd_cs;
 	end
