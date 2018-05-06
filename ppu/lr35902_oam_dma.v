7240467f031bb85652050db30b6a11cfe0b70653
Michael Singer <michael@a-singer.de>
2018-05-06 17:26:30 +0200

    fix OAM DMA timing

--- ppu/lr35902_oam_dma.v
+++ ppu/lr35902_oam_dma.v
@@ -1,8 +1,9 @@
 `default_nettype none
 
 `define state_idle  0
-`define state_setup 1
-`define state_busy  2
+`define state_start 1
+`define state_setup 2
+`define state_busy  3
 
 (* nolatches *)
 module lr35902_oam_dma(
@@ -26,44 +27,53 @@ module lr35902_oam_dma(
 	reg [7:0] r_pos;   wire [7:0] pos;
 	reg [7:0] r_sadr;  wire [7:0] sadr;
 
+	reg r_active;
+
 	assign dout    = din;
 	assign adr     = { sadr, pos };
 	assign adr_oam = pos;
 	assign read    = state == `state_busy && cycle < 2;
 	assign write   = state == `state_busy && cycle < 2;
-	assign active  = state != `state_idle;
 
 	always @* begin
-		state = r_state;
-		cycle = r_cycle + 1;
-		pos   = (&r_cycle && r_state == `state_busy) ? (r_pos + 1) : r_pos;
-		sadr  = r_sadr;
+		state  = r_state;
+		cycle  = r_cycle + 1;
+		pos    = (&r_cycle && r_state == `state_busy) ? (r_pos + 1) : r_pos;
+		sadr   = r_sadr;
+		active = r_active;
 
-		if (pos == 160)
+		if (r_cycle == 2 && r_pos == 159) begin
 			state = `state_idle;
+			active = 0;
+		end else if (&r_cycle && r_state == `state_start)
+			state = `state_setup;
 		else if (&r_cycle && r_state == `state_setup)
 			state = `state_busy;
+		else if (r_cycle == 2 && r_state == `state_setup)
+			active = 1;
 
 		if (r_reg_write && !reg_write) begin
-			state = `state_setup;
+			state = `state_start;
 			cycle = 2;
 			pos   = 0;
 			sadr  = reg_din;
 		end
 
 		if (reset) begin
-			state = `state_idle;
-			cycle = 'bx;
-			pos   = 'bx;
-			sadr  = 'bx;
+			state  = `state_idle;
+			cycle  = 'bx;
+			pos    = 'bx;
+			sadr   = 'bx;
+			active = 0;
 		end
 	end
 
 	always @(posedge clk) begin
-		r_state <= state;
-		r_cycle <= cycle;
-		r_pos   <= pos;
-		r_sadr  <= sadr;
+		r_state  <= state;
+		r_cycle  <= cycle;
+		r_pos    <= pos;
+		r_sadr   <= sadr;
+		r_active <= active;
 
 		r_reg_write <= reg_write;
 
