15a23fa24b55f663376206cef450564c934c012a
Michael Singer <michael@a-singer.de>
2021-01-27 22:04:02 +0100

    Make SM83 sim configurable with cmdline options

--- sim/sm83_sim.cc
+++ sim/sm83_sim.cc
@@ -1,11 +1,19 @@
 #include <cstdint>
+#include <getopt.h>
 #include <iostream>
 #include <iomanip>
+#include <string>
 #include "sm83_sim_design.h"
 
-struct sm83_mem {
-	uint8_t rom[32768];
-	uint8_t ram[32768];
+class sm83_mem {
+	uint16_t wr_adr, wr_mask;
+	uint8_t  ram[65536];
+
+public:
+	sm83_mem(uint16_t wr_adr, uint16_t wr_mask) : wr_adr(wr_adr), wr_mask(wr_mask), ram() { }
+	void init(uint16_t adr, uint8_t d) { ram[adr] = d; }
+	void write(uint16_t adr, uint8_t d) { if ((adr & wr_mask) == wr_adr) ram[adr] = d; }
+	uint8_t read(uint16_t adr) { return ram[adr]; }
 };
 
 static void step(sm83_sim_design::p_sm83& sm83)
@@ -75,27 +83,60 @@ static void edge(bool e)
 static void dio(sm83_sim_design::p_sm83& sm83, sm83_mem& mem)
 {
 	uint16_t adr(sm83.p_adr.get<uint16_t>());
-	if (sm83.p_p__rd.get<bool>()) {
-		if (adr & 0x8000)
-			sm83.p_din.set<uint16_t>(mem.ram[adr & 0x7fff]);
-		else
-			sm83.p_din.set<uint16_t>(mem.rom[adr]);
-	}
-	if (sm83.p_p__wr.get<bool>()) {
-		if (adr & 0x8000)
-			mem.ram[adr & 0x7fff] = sm83.p_dout.get<uint16_t>();
-	}
+	if (sm83.p_p__rd.get<bool>())
+		sm83.p_din.set<uint8_t>(mem.read(adr));
+	if (sm83.p_p__wr.get<bool>())
+		mem.write(adr, sm83.p_dout.get<uint8_t>());
 }
 
 int main(int argc, char** argv)
 {
-	sm83_mem mem;
+	int      ticks(32), rticks(4);
+	uint16_t wr_adr(0x8000), wr_mask(0x8000);
+	bool     bin_in(false);
 
-	for (int i = 0; i < sizeof mem.rom / sizeof *mem.rom; i++) {
-		int c;
-		std::cin >> std::hex >> c;
+	static const option ops[] = {
+		{ "binary",        no_argument,       NULL, 'b' },
+		{ "ticks",         required_argument, NULL, 't' },
+		{ "reset-ticks",   required_argument, NULL, 'r' },
+		{ "write-address", required_argument, NULL, 0   },
+		{ "write-mask",    required_argument, NULL, 1   },
+		{ NULL,            0,                 NULL, 0   }
+	};
+
+	int i, c;
+
+	while ((c = getopt_long(argc, argv, "bt:r:", ops, &i)) != -1) {
+		switch (c) {
+		case 'b':
+			bin_in = true;
+			break;
+		case 't':
+			ticks = std::stoi(optarg, NULL, 0);
+			break;
+		case 'r':
+			rticks = std::stoi(optarg, NULL, 0);
+			break;
+		case 0:
+			wr_adr = std::stoi(optarg, NULL, 0);
+			break;
+		case 1:
+			wr_mask = std::stoi(optarg, NULL, 0);
+			break;
+		default:
+			break;
+		}
+	}
+
+	sm83_mem mem(wr_adr, wr_mask);
+
+	for (i = 0; i < 65536; i++) {
+		if (bin_in)
+			c = std::cin.get();
+		else
+			std::cin >> std::hex >> c;
 		if (std::cin.eof()) break;
-		mem.rom[i] = c;
+		mem.init(i, c);
 	}
 
 	sm83_sim_design::p_sm83 sm83;
@@ -110,7 +151,7 @@ int main(int argc, char** argv)
 	std::cout << "[RESET asserted]" << std::endl;
 	debug(sm83);
 
-	for (int i = 0; i < 4; i++) {
+	for (i = 0; i < rticks; i++) {
 		sm83.p_clk.set<bool>(true);
 		dio(sm83, mem);
 		step(sm83);
@@ -125,7 +166,7 @@ int main(int argc, char** argv)
 	sm83.p_reset.set<bool>(false);
 	std::cout << "[RESET deasserted]" << std::endl;
 
-	for (int i = 0; i < 78; i++) {
+	for (i = 0; i < ticks; i++) {
 		bool rd(false), wr(false);
 		if (get_t(sm83) == 4) {
 			rd = sm83.p_dbg__mread.get<bool>();
