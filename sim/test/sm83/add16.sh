3fb36e81685dbece7c2fa754744c7eb9492f60e5
Michael Singer <michael@a-singer.de>
2021-02-12 20:05:49 +0100

    SM83: Initialize AF for add16 tests

--- sim/test/sm83/add16.sh
+++ sim/test/sm83/add16.sh
@@ -12,33 +12,33 @@ set -e
 
 TEST=sm83_sim_add16
 
-simulate 208 <<"EOF"
+simulate 220 <<"EOF"
 # @tick #0
 # Preset registers with values
-# LD HL, $8000; LD SP, HL; LD HL, $0080; LD BC, $0181; LD DE, $0282
-21 00 80 f9 21 80 00 01 81 01 11 82 02
-# 56 ticks
+# LD HL, $7ffe; LD SP, HL; LD HL, $0080; LD BC, $0181; LD DE, $0282; POP AF
+21 fe 7f f9 21 80 00 01 81 01 11 82 02 f1
+# 68 ticks
 
-# @tick #56
+# @tick #68
 # ADD HL, HL; ADD HL, BC; ADD HL, DE; ADD HL, SP; ADD HL, HL; ADD HL, HL; LD HL, $0000; ADD HL, HL
 29 09 19 39 29 29 21 00 00 29
 # 68 ticks
 
-# @tick #124
+# @tick #136
 # INC HL; DEC HL; DEC HL; INC BC; DEC DE; DEC SP
 23 2b 2b 03 1b 3b
 # 48 ticks
 
-# @tick #172
+# @tick #184
 # ADD SP, 5; ADD SP, -100
 e8 05 e8 9c
 # 32 ticks
 EOF
 
 compare_mem <<"EOF"
-0000000    0021    f980    8021    0100    0181    8211    2902    1909
-0000010    2939    2129    0000    2329    2b2b    1b03    e83b    e805
-0000020    009c    0000    0000    0000    0000    0000    0000    0000
+0000000    fe21    f97f    8021    0100    0181    8211    f102    0929
+0000010    3919    2929    0021    2900    2b23    032b    3b1b    05e8
+0000020    9ce8    0000    0000    0000    0000    0000    0000    0000
 0000030    0000    0000    0000    0000    0000    0000    0000    0000
 *
 0010000
@@ -46,23 +46,23 @@ EOF
 
 grep_output <<"EOF"
 # Check ADD HL, dd
-67@posedge     M=1 T=4    .* SP=0x8000 BC=0x0181 DE=0x0282 HL=0x0100 AF=0x0000
-75@posedge     M=1 T=4    .* SP=0x8000 BC=0x0181 DE=0x0282 HL=0x0281 AF=0x0000
-83@posedge     M=1 T=4    .* SP=0x8000 BC=0x0181 DE=0x0282 HL=0x0503 AF=0x0000
-91@posedge     M=1 T=4    .* SP=0x8000 BC=0x0181 DE=0x0282 HL=0x8503 AF=0x0000
-99@posedge     M=1 T=4    .* SP=0x8000 BC=0x0181 DE=0x0282 HL=0x0a06 AF=0x0010
-107@posedge     M=1 T=4    .* SP=0x8000 BC=0x0181 DE=0x0282 HL=0x140c AF=0x0020
-127@posedge     M=1 T=4    .* SP=0x8000 BC=0x0181 DE=0x0282 HL=0x0000 AF=0x0000
+79@posedge     M=1 T=4    .* SP=0x8000 BC=0x0181 DE=0x0282 HL=0x0100 AF=0x0000
+87@posedge     M=1 T=4    .* SP=0x8000 BC=0x0181 DE=0x0282 HL=0x0281 AF=0x0000
+95@posedge     M=1 T=4    .* SP=0x8000 BC=0x0181 DE=0x0282 HL=0x0503 AF=0x0000
+103@posedge     M=1 T=4    .* SP=0x8000 BC=0x0181 DE=0x0282 HL=0x8503 AF=0x0000
+111@posedge     M=1 T=4    .* SP=0x8000 BC=0x0181 DE=0x0282 HL=0x0a06 AF=0x0010
+119@posedge     M=1 T=4    .* SP=0x8000 BC=0x0181 DE=0x0282 HL=0x140c AF=0x0020
+139@posedge     M=1 T=4    .* SP=0x8000 BC=0x0181 DE=0x0282 HL=0x0000 AF=0x0000
 
 # Check INC/DEC dd
-135@posedge     M=1 T=4    .* SP=0x8000 BC=0x0181 DE=0x0282 HL=0x0001 AF=0x0000
-143@posedge     M=1 T=4    .* SP=0x8000 BC=0x0181 DE=0x0282 HL=0x0000 AF=0x0000
-151@posedge     M=1 T=4    .* SP=0x8000 BC=0x0181 DE=0x0282 HL=0xffff AF=0x0000
-159@posedge     M=1 T=4    .* SP=0x8000 BC=0x0182 DE=0x0282 HL=0xffff AF=0x0000
-167@posedge     M=1 T=4    .* SP=0x8000 BC=0x0182 DE=0x0281 HL=0xffff AF=0x0000
-175@posedge     M=1 T=4    .* SP=0x7fff BC=0x0182 DE=0x0281 HL=0xffff AF=0x0000
+147@posedge     M=1 T=4    .* SP=0x8000 BC=0x0181 DE=0x0282 HL=0x0001 AF=0x0000
+155@posedge     M=1 T=4    .* SP=0x8000 BC=0x0181 DE=0x0282 HL=0x0000 AF=0x0000
+163@posedge     M=1 T=4    .* SP=0x8000 BC=0x0181 DE=0x0282 HL=0xffff AF=0x0000
+171@posedge     M=1 T=4    .* SP=0x8000 BC=0x0182 DE=0x0282 HL=0xffff AF=0x0000
+179@posedge     M=1 T=4    .* SP=0x8000 BC=0x0182 DE=0x0281 HL=0xffff AF=0x0000
+187@posedge     M=1 T=4    .* SP=0x7fff BC=0x0182 DE=0x0281 HL=0xffff AF=0x0000
 
 # Check ADD SP, e
-191@posedge     M=1 T=4    .* SP=0x8004 BC=0x0182 DE=0x0281 HL=0xffff AF=0x0020
-207@posedge     M=1 T=4    .* SP=0x7fa0 BC=0x0182 DE=0x0281 HL=0xffff AF=0x0010
+203@posedge     M=1 T=4    .* SP=0x8004 BC=0x0182 DE=0x0281 HL=0xffff AF=0x0020
+219@posedge     M=1 T=4    .* SP=0x7fa0 BC=0x0182 DE=0x0281 HL=0xffff AF=0x0010
 EOF
