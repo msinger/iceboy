3aaa58140faa5bd1621ae06a75d7c805deed2be0
Michael Singer <michael@a-singer.de>
2021-02-21 03:43:00 +0100

    SM83: Fix carry output of SRA

--- src/cpu/sm83_control.sv
+++ src/cpu/sm83_control.sv
@@ -2023,7 +2023,7 @@ module sm83_control(
 
 					m1 && t3: begin
 						/* Write ALU flags into register F */
-						ctl_alu_fl_sel_c2    = 1;
+						ctl_alu_fl_sel_c2    = opcode[5:3] != 5; /* set carry to shift out; except for SRA */
 						f_from_alu();
 					end
 				endcase
